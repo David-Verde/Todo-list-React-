c9aec7a68e2679a5cf2d30620c5b2db8653fdf48
David-Verde <david.verde.alvarez@gmail.com>
2025-01-30 18:01:06 -0500

    fix login

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Navbar from "./components/common/Navbar";
 import Login from "./components/pages/Login";
@@ -7,16 +7,15 @@ import Dashboard from "./components/pages/Dashboard";
 import PrivateRoute from "./components/common/PrivateRoute";
 import Footer from "./components/pages/Footer";
 
-
 function App() {
   return (
     <BrowserRouter>
- 
       <Navbar />
-
       <Toaster position="top-right" />
 
       <Routes>
+       
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route
@@ -24,12 +23,10 @@ function App() {
           element={
             <PrivateRoute>
               <Dashboard />
-         
             </PrivateRoute>
           }
         />
       </Routes>
-
 
       <Footer />
     </BrowserRouter>
